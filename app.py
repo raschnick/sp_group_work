28e68966b36ecf0a17c9ada83d51795e71c5df43
simon <eglisi1@students.zhaw.ch>
2022-03-23 15:36:56 +0100

    add 404 handler

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+# Load Configurations
+app.config.from_object('config')
 
 
 @app.route('/')
@@ -8,5 +10,10 @@ def hello_world():
     return 'Hello, can someone here me? I am stuck in a container!'
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
