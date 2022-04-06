bf05f3ef3e5259f1946e244b409e861dccb8bdb3
simon <eglisi1@students.zhaw.ch>
2022-04-06 16:16:53 +0200

    use db user and pw from environment

--- service/db_service.py
+++ service/db_service.py
@@ -21,18 +21,18 @@ class DbService:
         except KeyError:
             print('You have to define a Mongo User (DB_USER) and Password (DB_PASS) in your environment')
             exit()
-        client = MongoClient(
+        client = MongoClient(host=
             f"mongodb://{username}:{password}@cluster0-shard-00-00.mkt3y.mongodb.net:27017,cluster0-shard-00-01.mkt3y.mongodb.net:27017,cluster0-shard-00-02.mkt3y.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-46zapx-shard-0&authSource=admin&retryWrites=true&w=majority")
         self.db = client['portfolio_management']
 
-    def setup_db(self):
+    def setup_db(self) -> None:
         if self.get_amount_documents() == 0:
             self.insert_sample_data()
 
     def get_amount_documents(self, collection: str = 'depot') -> int:
         return self.db[collection].find().count()
 
-    def insert_sample_data(self):
+    def insert_sample_data(self) -> None:
         collections = ['currency', 'depot', 'transaction']
         dirname = os.path.dirname(__file__)
         for collection in collections:
